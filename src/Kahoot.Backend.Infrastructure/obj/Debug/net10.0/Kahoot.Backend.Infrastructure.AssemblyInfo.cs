3d7c37f0d131f97c7fcb6bcbe37d84f00bed3b3a
ismailcanuslu <isocan5561@gmail.com>
2026-05-08 15:46:01 +0300

    ci actionds added

--- src/Kahoot.Backend.Infrastructure/obj/Debug/net10.0/Kahoot.Backend.Infrastructure.AssemblyInfo.cs
+++ src/Kahoot.Backend.Infrastructure/obj/Debug/net10.0/Kahoot.Backend.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kahoot.Backend.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a1b3c1f24170b3ee8e2bad6f2be0906de48d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kahoot.Backend.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kahoot.Backend.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
